0c2d1f6e17ff8a6f5776442cc32eb2c9c641e94f
isaiasleibo1 <isaias@leibovich.ar>
2023-02-02 12:00:43 -0300

    .

--- htdocs/js/verItem.js
+++ htdocs/js/verItem.js
@@ -24,7 +24,7 @@ const camara = document.querySelector('#camara');
 
 
 const request = new XMLHttpRequest();
-request.open('GET', './js/json/items.json');
+request.open('GET', '../js/json/items.json');
 request.responseType = 'json';
 request.send();
 
